b64ddf07f081003029aba1df9e4374a92a0f6638
heroccccc <heroccc5@gmail.com>
2021-01-29 00:28:21 +0900

    change appp.py to connect rds

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.dialects.mysql import INTEGER, VARCHAR
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
+      'user': "root",
+      'password': "Abcde5Fghij1Klmno1Pqrst",
+      'host': "sample-db-1.caruzcbh5rmg.ap-northeast-1.rds.amazonaws.com",
+      'db_name': "test"
+  })
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Test_sample1(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.Text)
+    value1 = db.Column(db.Integer)
+    valu2 = db.Column(db.Integer)
+
+
 @app.route('/')
 def get_sample():
-    name = "TAKAHASHI"
-    return render_template('sample1.html', name=name)
+    data = Test_sample1.query.filter_by(id=1).first()
+    return render_template('sample1.html', name=data.name)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
